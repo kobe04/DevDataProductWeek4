cf52f9e677250233254c839bd61ef716c1da4f88
Koen <47666456+kobe04@users.noreply.github.com>
2019-03-24 09:06:05 +0100

    Adjusted some details

--- myFirstApp/server.R
+++ myFirstApp/server.R
@@ -4,6 +4,8 @@ library(ggplot2)
 # Define server logic required to draw plot and regression line
 shinyServer(function(input, output) {
     output$plot1 <- renderPlot({
+        data("swiss")
+        varNames <- names(swiss[,-1])
         listNames <- c("Agriculture", "Examination", "Education", "Catholic", "Infant.Mortality")
         labelx <- which(varNames %in% input$predictor)
         labelx <- listNames[labelx]
@@ -13,7 +15,7 @@ shinyServer(function(input, output) {
         g <- ggplot(swiss, aes(x = xvar, y = Fertility)) + geom_point(aes(colour = "red"),
                                                                       show.legend = FALSE)
         g <- g + labs(x = labelx, title = paste(labelx,
-                                                " versus Fertility, with linear regression line"))
+                                                "versus Fertility, with linear regression line"))
         g <- g + theme_bw() + geom_smooth(method = "lm")
         g
   })
